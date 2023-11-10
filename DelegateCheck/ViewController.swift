cc5791032300d26b7a4283049509a0afe12b49a6
Supreet <psupreet12@gmail.com>
2023-11-10 17:28:36 +0530

    Initial Commit

---
+++ DelegateCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateCheck
+//
+//  Created by Supreet on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
